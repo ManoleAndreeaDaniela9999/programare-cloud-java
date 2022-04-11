860fe82303ffda2935748e6f7dd743de068ee5b2
Purple6666 <sirpurple2016@yahoo.com>
2022-04-11 08:59:27 +0300

    corrections

--- lab-6-api-gateway/service2/src/main/java/com/example/service2/Product.java
+++ lab-6-api-gateway/service2/src/main/java/com/example/service2/Product.java
@@ -8,21 +8,10 @@ import org.jetbrains.annotations.NotNull;
 import javax.persistence.*;
 import java.io.Serializable;
 
-@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 public class Product implements Serializable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", nullable = false,updatable = false)
-    private Long id;
-
-    @NotNull
-    @Column(name = "name",nullable = false, unique = true)
     private String name;
-
-    @NotNull
-    @Column(name = "quantity",nullable = false, unique = true)
     private int quantity;
 }
